e544d4e0f7311a29bf38f2298be4ac69ce337d60
Quyen <bntquyenn@gmail.com>
2023-07-17 13:57:01 +0700

    sample

--- src/main/java/com/example/demo/Security/SecurityConfig.java
+++ src/main/java/com/example/demo/Security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 .authorizeRequests()
-                .requestMatchers("api/v1/**")
+                .requestMatchers("api/v1/login")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
